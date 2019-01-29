b696420ebc7749ac95d14445b949e8c93bdbcae8
pyuz <fiveunderscores@protonmail.com>
2019-01-29 11:02:48 -0500

    anonymize

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+document.getElementById("fake").addEventListener("click", makesColor);
+
+//get off your ass
+function makesColor() {
+  document.getElementById("fake").style.color = "#ff3333";
+}
