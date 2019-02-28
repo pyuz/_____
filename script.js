e86db137a0d1ad26eaf113be0d18cbecb23e5b2f
pyuz <fiveunderscores@protonmail.com>
2019-02-27 22:21:39 -0500

    add 007

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
 document.getElementById("fake").addEventListener("click", revealSecret);
 
 function revealSecret() {
-  document.getElementById("fake").style.color = "#ff7799";
+  document.getElementById("fake").style.color = "#f79";
   document.getElementById("secret").style.visibility = "visible";
+}
+
+function hideSecret() {
+  document.getElementById("fake").style.color = "#49f";
+  document.getElementById("secret").style.visibility = "hidden";
 }
